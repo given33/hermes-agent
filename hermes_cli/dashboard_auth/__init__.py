a34a7837e1904fc40a389abb59343a06bc4c5219
given33 <3318766864@qq.com>
2026-09-02 21:34:32 +0800

    fix(auth): distinguish opaque bearers from provider outages

--- hermes_cli/dashboard_auth/__init__.py
+++ hermes_cli/dashboard_auth/__init__.py
@@ -19,6 +19,7 @@ from hermes_cli.dashboard_auth.base import (
     ProviderError,
     RefreshExpiredError,
     assert_protocol_compliance,
+    classify_jwks_lookup_error,
 )
 from hermes_cli.dashboard_auth.registry import (
     register_provider,
@@ -40,6 +41,7 @@ __all__ = [
     "ProviderError",
     "RefreshExpiredError",
     "assert_protocol_compliance",
+    "classify_jwks_lookup_error",
     "register_provider",
     "get_provider",
     "list_providers",
